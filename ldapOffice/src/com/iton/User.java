eae2949c608af4f5bbc16b1308770a0b17ed0bcb
kiran <kiran.kanaka@myiton.com>
2012-12-05 10:10:48 +0530

    First Commit!

---
+++ ldapOffice/src/com/iton/User.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.iton;
+
+/**
+ * @author Administrator
+ *
+ */
+public class User {
+
+	int uid;
+
+	public int getUid() {
+		return uid;
+	}
+
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+	
+}
